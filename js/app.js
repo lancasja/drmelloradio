3917910f797517a13951c42ccb5139d0e8fa37f0
lancasja <lancasja@gmail.com>
2015-03-19 21:30:44 -0700

    Added info buttons on grid items.

--- js/app.js
+++ js/app.js
@@ -16,4 +16,27 @@
 		removeTop();
 		$(this).toggleClass('on-top');
 	});
+
+
+	var $detailButton = $('.grid-item .more-info');
+	var $detailPane = $('#sc01-detail');
+	
+	$detailButton.mouseover(function() {
+		$detailPane.css('display', 'block');
+
+		$detailPane.animate({
+			opacity: 1
+		}, 500, function() {
+			console.log('Details shown.');
+		});
+	});
+
+	$detailButton.mouseleave(function() {
+		$detailPane.animate({
+			opacity: 0
+		}, 500, function() {
+			$detailPane.css('display', 'none');
+			console.log('Details hidden.');
+		});
+	});
 })();
